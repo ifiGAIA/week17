69f35dbbb52ae3c69fa607610d60289fb77813df
ifiGAIA <39110447+ifiGAIA@users.noreply.github.com>
2018-06-20 11:01:27 +0800

    Add files via upload

---
+++ sketch_jun20a/sketch_jun20a.ino
@@ -0,0 +1,18 @@
+int ledpin = 13;
+int intpin = 2;
+int state = 0;
+
+void setup() {
+  pinMode(ledpin, OUTPUT);
+  pinMode(ledpin, INPUT);
+  digitalWrite(ledpin, state);
+  attachInterrupt(0, int0, LOW);
+
+}
+
+void loop() {
+  digitalWrite(ledpin, state);
+}
+void int0() {
+  state = !state;
+}
